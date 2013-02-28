03312d5d5320a0a14b379062e9976af4a02b837d
Spencer Kelly <spencermountain@gmail.com>
2013-02-28 18:23:18 +0000

    word

--- README.md
+++ README.md
@@ -1,4 +1,17 @@
 Dirty-prototype-manipulation
 ==========================
 
-where i create bugs in mass force
+its like prototype or underscore, but it appends the built-in prototypes, which is bad practice
+
+but dammit if this doesn't feel good:
+  
+     var data=[1,2,3,3,null,[3]]
+     data.uniq().flatten().sort().print()
+
+
+     npm install dirty
+
+     var dirty=require("dirty")
+
+to clean-up the mess afterwards:
+     dirty.cleanup()
--- index.js
+++ index.js
@@ -1,39 +1,114 @@
-Array.prototype.grab=function(p){
-	return this.map(function(s){
-		return s[p]
+//make a mess
+
+var dirty=(function(){
+	var dirty={};
+
+	var fns={}
+
+	fns.grab=function(p){
+							return this.map(function(s){
+								return s[p]
+							})
+						}
+	fns.collect=fns.grab;
+
+//nulls or undefined
+	fns.uniq=function(){
+						 	 var x=this;
+						 	 if(!field){
+							   var newArray=new Array();
+							    label:for(var i=0; i<x.length;i++ ){
+							      for(var j=0; j<newArray.length;j++ ){ 
+							          if(newArray[j]==x[i]) 
+							          continue label;
+							        }
+							        newArray[newArray.length] = x[i];
+							      }
+							    return newArray;
+							  }else{
+							   var newArray=new Array();
+							    label:for(var i=0; i<x.length;i++ ){
+							      for(var j=0; j<newArray.length;j++ ){ 
+							          if(newArray[j][field]==x[i][field]) 
+							          continue label;
+							        }
+							        newArray[newArray.length] = x[i];
+							      }
+							    return newArray;
+						  	}
+						  }
+	fns.unique=fns.uniq
+
+		//remove nulls
+	fns.compact=function(){
+					return this.filter(function(v){return v===0||v})
+				}
+
+		//remove nested arrays one step
+	fns.flatten=function(){
+					    var flat = [];
+					    var array=this;
+					    for (var i = 0, l = array.length; i < l; i++){
+					        var type = Object.prototype.toString.call(array[i]).split(' ').pop().split(']').shift().toLowerCase();
+					        if (type) { flat = flat.concat(/^(array|collection|arguments|object)$/.test(type) ? Array.prototype.flatten(array[i]) : array[i]); }
+					    }
+					    return flat;
+					}
+
+		//pretty-print
+	fns.print=function(){
+				console.log(JSON.stringify(this, null, 2));
+			}
+	fns.printf=fns.print
+	fns.console=fns.print
+	fns.log=fns.print
+
+
+	//add them to the prototype
+	Object.keys(fns).forEach(function(i){
+		Object.defineProperty(Array.prototype, i, {
+			value: fns[i],
+			configurable: true,
+		  enumerable: false
+		});
 	})
-}
 
- //remove duplicates from array
- Array.prototype.uniq=function(){
- 	 var x=this;
- 	 if(!field){
-	   var newArray=new Array();
-	    label:for(var i=0; i<x.length;i++ ){
-	      for(var j=0; j<newArray.length;j++ ){ 
-	          if(newArray[j]==x[i]) 
-	          continue label;
-	        }
-	        newArray[newArray.length] = x[i];
-	      }
-	    return newArray;
-	  }else{
-	   var newArray=new Array();
-	    label:for(var i=0; i<x.length;i++ ){
-	      for(var j=0; j<newArray.length;j++ ){ 
-	          if(newArray[j][field]==x[i][field]) 
-	          continue label;
-	        }
-	        newArray[newArray.length] = x[i];
-	      }
-	    return newArray;
-  	}
-  }
 
-//pretty-print
- Array.prototype.print=function(){
- 	console.log(JSON.stringify(this, null, 2));
- }
+	//repair the array prototype
+	dirty.undo=function(){
+		Object.keys(fns).forEach(function(i){
+			Object.defineProperty(Array.prototype, i, {
+			    value: undefined
+			});
+		})
+	}
 
-//r=[3,4,3,4,5,6]
-//console.log(r.print())
+
+//export the module
+    // AMD / RequireJS
+    if (typeof define !== 'undefined' && define.amd) {
+        define([], function () {
+            return dirty;
+        });
+    }
+    // Node.js
+    else if (typeof module !== 'undefined' && module.exports) {
+        module.exports = dirty;
+    }
+
+    return dirty;
+
+})()
+
+
+//  r=[3,4,3,4,[5],6,0,null,7]
+//   r.flatten().compact().print()
+
+// dirty.undo()
+
+
+//   r.print()
+
+//  for(var i in r){
+//   console.log(i)
+//  }
